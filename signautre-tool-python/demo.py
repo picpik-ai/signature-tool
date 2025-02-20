c9ca4a9352c779ddfbfd8689b97a38bd6aaca801
Joshua.Yin <joshua.yin@foxmail.com>
2025-02-20 14:45:29 +0800

    python sdk

---
+++ signautre-tool-python/demo.py
@@ -0,0 +1,34 @@
+import json
+
+import picpik_signer
+
+# 示例使用
+if __name__ == "__main__":
+    public_key = "abcdefg"
+    private_key = "123456"
+    api_key = "ABCDEFG"
+
+    signer = picpik_signer.PicpikSigner(public_key, private_key, api_key)
+    service_payload = {
+        "prompt": "这是生成图片所需的提示词。",
+        "width": 512,
+        "height": 512,
+        "refImage": "如果是图生图，此处填原图的base64字符串"
+    }
+
+    # 用于 picpik AI业务的API
+    signature = signer.sign_service(service_payload)
+    service_payload['signature'] = signature
+
+    print(f"payload={json.dumps(service_payload, indent=4, ensure_ascii=False)}\n")
+
+    console_payload = {
+        "Action": "ListModels",
+        "PublicKey": public_key
+    }
+
+    # 用于操作管理 picpik 实例节点的API
+    signature = signer.sign_platform(console_payload)
+    console_payload['signature'] = signature
+
+    print(f"payload2={json.dumps(console_payload, indent=4, ensure_ascii=False)}\n")
